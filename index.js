44f60b3f0bb2558bf30b71d33bab672b9cdee3f0
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-26 13:43:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function divide(a,b) {
   return a / b
 }
 function increment(n) {
-  return n++
+  return n+=1
 }
 function decrement(n) {
-  return n--
+  return n-=1
 }
 function makeInt(n) {
   return parseInt('n',10)
